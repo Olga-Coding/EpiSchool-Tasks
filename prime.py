c46fbd1d2583f9eaef9a16ac4f6f81fca847eb60
Olga-Coding <99608480+Olga-Coding@users.noreply.github.com>
2022-05-05 19:00:46 +0300

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+def my_range(m,n):
+    m=0
+    while m+1 < n:
+        def is_prime(num):
+            n = num % n
+            if (n == 0):
+                return ('True')
+            else:
+                return ('False')
+print (my_range)
