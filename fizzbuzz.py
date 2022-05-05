8e9168e62c8a04e64a6367a5fbb457e55e8decf0
Olga-Coding <99608480+Olga-Coding@users.noreply.github.com>
2022-05-05 18:35:42 +0300

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+def my_range(m,n):
+    m=1
+    n=1000
+    while m < n:
+        def n (num):
+            n == num % 3
+            if (n != 0):
+                n == num % 5
+            if (n != 0):
+                print(n)
+            else:
+                n == num % 3
+                if (n == 0):
+                    n == num % 5
+                if (n == 0):
+                    print('FizzBuzz')
+                else:
+                    n == num % 3
+                    if (n == 0):    
+                        print('Fizz')
+                    else:
+                        print('Buzz')
+  
